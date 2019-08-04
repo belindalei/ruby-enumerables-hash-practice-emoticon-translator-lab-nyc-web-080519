af88fc7ee1c88615a8e41b71e2a0f4dba92769ac
Belinda Lei <belinda.lei95@gmail.com>
2019-08-04 07:19:34 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -4,8 +4,13 @@ def load_library(path)
   hash = YAML.load_file(path)
 end
 
-def get_japanese_emoticon
-  # code goes here
+def get_japanese_emoticon(emoticon)
+  hash = load_library("emoticons.yml")
+  hash.each do |meaning, symbols|
+    if symbols[0] === emoticon
+      return symbols[1]
+    end
+  end
 end
 
 def get_english_meaning
