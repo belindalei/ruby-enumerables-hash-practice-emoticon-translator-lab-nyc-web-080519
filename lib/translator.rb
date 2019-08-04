d8972c1455cbadb401444f91d4d13f23d0f749b7
Belinda Lei <belinda.lei95@gmail.com>
2019-08-04 07:32:10 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -15,9 +15,7 @@ def get_japanese_emoticon(path, emoticon)
   return hash[:get_emoticon][emoticon]
 end
 
-def get_english_meaning
-  def get_english_meaning(path, emoticon)
+def get_english_meaning(path, emoticon)
   hash = load_library(path)
   return hash[:get_emoticon][emoticon]
-end
 end
